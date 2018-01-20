97284462738c28a8fda4cd1e58ee14ab802836a6
shahriar <30356865+sshahriar@users.noreply.github.com>
2018-01-21 04:38:43 +0600

    Create cp_tmeplate.cpp

---
+++ cp_tmeplate.cpp
@@ -0,0 +1,37 @@
+//dont let your dreams be dreams 
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define M 100005
+#define mx 5000000
+#define mp make_pair
+#define pb push_back
+#define pii pair<int , int>
+#define rep(i , n) for(i = 0 ; i< n ;i++ )
+#define repi(i , n ) for(i =1  ; i<= n ; i++ )
+#define mem(x , y ) memset(x , y , sizeof x )
+#define ff first 
+#define ss second
+#define sf1(x)  scanf("%d", &x ) ;
+#define sf2(x, y  ) scanf("%d%d",  &x, &y) ;
+
+typedef long long ll ; 
+
+//int dx[] = { -1 ,+1, 0 , 0 , -1 , -1  , 1 , 1 };
+//int dy[] = { 0 , 0, +1, -1  , +1 , -1 , 1 , -1 };
+
+//int dx[] = { 1 , 1, 2 , 2  , -1 , -1 , -2 , -2 }; // knight direction 
+//int dy[] = { 2 , -2, +1, -1 ,2 , -2 , 1 , -1 };
+
+vector< int > g[M] ; 
+int arr[101]  ;
+
+int main(){
+//	freopen("in.txt", "r",stdin );
+
+	int i , j , k , cs= 0 , tc ;
+
+	return 0;
+}
