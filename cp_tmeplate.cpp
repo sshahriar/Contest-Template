1639aeba20a000611598f1950185d2ae55120ed5
shahriar <30356865+sshahriar@users.noreply.github.com>
2018-04-11 03:03:17 +0600

    Update cp_tmeplate.cpp

--- cp_tmeplate.cpp
+++ cp_tmeplate.cpp
@@ -1,37 +1,56 @@
-//dont let your dreams be dreams 
+#include <bits/stdc++.h>
 
-#include<bits/stdc++.h>
+using namespace std ;
 
-using namespace std;
-
-#define M 100005
-#define mx 5000000
+#define	M 	100000
+#define inf  1 << 30
 #define mp make_pair
 #define pb push_back
-#define pii pair<int , int>
-#define rep(i , n) for(i = 0 ; i< n ;i++ )
-#define repi(i , n ) for(i =1  ; i<= n ; i++ )
-#define mem(x , y ) memset(x , y , sizeof x )
-#define ff first 
+#define ff first
 #define ss second
+#define pii pair<int , int>
+#define rep(i , n) for(int  i = 0 ; (i) < n ; i++ )
+#define repi(i , n ) for(int   i = 1  ; i <= n ; i++ )
+#define mem(x , y ) memset(x , y , sizeof x )
 #define sf1(x)  scanf("%d", &x ) ;
 #define sf2(x, y  ) scanf("%d%d",  &x, &y) ;
+#define all(a ) a.begin() , a.end()
+#define _lb(x ,y ) lower_bound(all(x  ) , y ) - x.begin
+#define fast ios_base :: sync_with_stdio(false ) ;  cin.tie(0) ; 
+#define dbg(x )  cerr << #x << " : " << x << endl 
+#define dbg2(x, y ) cerr << #x << " : " << x << " " << #y << " : " << y  << endl ; 
+#define dbg3(x, y ,z ) dbg2(x,y  ) cerr << " " << #z << " : " << z << endl; 
 
-typedef long long ll ; 
+
+int _set(int n, int pos  ) { return n = n|(1<< pos) ; }
+int reset(int n,int pos ) {  return n =  n & ~(1<<pos ); }
+bool check(int n,int pos ) { return (bool ) (n& (1<<pos))  ; }
+
 
 //int dx[] = { -1 ,+1, 0 , 0 , -1 , -1  , 1 , 1 };
 //int dy[] = { 0 , 0, +1, -1  , +1 , -1 , 1 , -1 };
 
-//int dx[] = { 1 , 1, 2 , 2  , -1 , -1 , -2 , -2 }; // knight direction 
+//int dx[] = { 1 , 1, 2 , 2  , -1 , -1 , -2 , -2 };
 //int dy[] = { 2 , -2, +1, -1 ,2 , -2 , 1 , -1 };
 
-vector< int > g[M] ; 
-int arr[101]  ;
 
-int main(){
-//	freopen("in.txt", "r",stdin );
+typedef long long ll ;
 
-	int i , j , k , cs= 0 , tc ;
 
-	return 0;
+
+int main()
+{
+    fast
+
+    //freopen("in.txt", "r",stdin );
+    // freopen("out.txt", "w",stdout );
+
+    int k, cs = 0, tc , n   ;
+   
+    
+
+    return 0;
+
 }
+
+
