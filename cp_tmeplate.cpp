bd8b7d872f13a7e0b3ad1bad25c7f407f5f4aa8b
shahriar <30356865+sshahriar@users.noreply.github.com>
2018-04-30 20:47:48 +0600

    Update cp_tmeplate.cpp

--- cp_tmeplate.cpp
+++ cp_tmeplate.cpp
@@ -2,30 +2,29 @@
 
 using namespace std ;
 
-#define	M 	100000
-#define inf  1 << 30
+#define M 100000
+#define inf 1 << 30
 #define mp make_pair
 #define pb push_back
 #define ff first
 #define ss second
+#define sz(x ) x.size()
 #define pii pair<int , int>
-#define rep(i , n) for(int  i = 0 ; (i) < n ; i++ )
-#define repi(i , n ) for(int   i = 1  ; i <= n ; i++ )
+#define forn(i,a,n ) for(int  i = a ; i <= n ; i++ )
+#define rep(i ,n ) for(int i = 0 ; i < n ; i++ )
+#define repi(i ,n ) for(int i = 1 ; i <= n ; i++ )
+#define per(i , a, n ) for(int   i = a  ; i >= n ; i-- )
 #define mem(x , y ) memset(x , y , sizeof x )
-#define sf1(x)  scanf("%d", &x ) ;
-#define sf2(x, y  ) scanf("%d%d",  &x, &y) ;
 #define all(a ) a.begin() , a.end()
-#define _lb(x ,y ) lower_bound(all(x  ) , y ) - x.begin
-#define fast ios_base :: sync_with_stdio(false ) ;  cin.tie(0) ; 
-#define dbg(x )  cerr << #x << " : " << x << endl 
-#define dbg2(x, y ) cerr << #x << " : " << x << " " << #y << " : " << y  << endl ; 
-#define dbg3(x, y ,z ) dbg2(x,y  ) cerr << " " << #z << " : " << z << endl; 
+#define lowb(x ,y) lower_bound(all(x  ) , y ) - x.begin()
+#define uperb(x, y) upper_bound(all(x) ,y )-x.begin()
+#define printa(x,y ) rep(i,y ) cerr << x <<' ' ;  cerr << "\n"; 
 
+#define dbg(args...) do { print(args); } while(0); cerr<< endl;
 
-int _set(int n, int pos  ) { return n = n|(1<< pos) ; }
+int sett(int n, int pos  ) { return n = n|(1<< pos) ; }
 int reset(int n,int pos ) {  return n =  n & ~(1<<pos ); }
 bool check(int n,int pos ) { return (bool ) (n& (1<<pos))  ; }
-
 
 //int dx[] = { -1 ,+1, 0 , 0 , -1 , -1  , 1 , 1 };
 //int dy[] = { 0 , 0, +1, -1  , +1 , -1 , 1 , -1 };
@@ -33,20 +32,37 @@ bool check(int n,int pos ) { return (bool ) (n& (1<<pos))  ; }
 //int dx[] = { 1 , 1, 2 , 2  , -1 , -1 , -2 , -2 };
 //int dy[] = { 2 , -2, +1, -1 ,2 , -2 , 1 , -1 };
 
+//debug  extensions
+template<typename T >
+void print(const T& v) {
+    cerr << v << ' ' ;
+}
+
+template< typename FIRST, typename... REST >
+void print( const FIRST& first, const REST&... rest ){
+    print(first) ;
+    print(rest...) ;
+}
+
+template<typename T>
+ostream &operator<<(ostream &os, const vector<T> &v){
+    rep(i ,sz(v ) ) cerr << v[i] <<' ' ;
+    cerr<<endl;
+}
+
+// code starts
 
 typedef long long ll ;
 
-
+int a[M+2] ;
 
 int main()
 {
-    fast
-
+    //ios_base :: sync_with_stdio(false ) ;  cin.tie(0) ;
     //freopen("in.txt", "r",stdin );
     // freopen("out.txt", "w",stdout );
 
-    int k, cs = 0, tc , n   ;
-   
+    int k, cs=0, tc,n ;
     
 
     return 0;
