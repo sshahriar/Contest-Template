1e3255d8dc33b0ac198bb8607713206a621ffdd6
shahriar <30356865+sshahriar@users.noreply.github.com>
2019-03-11 11:13:24 +0600

    Create kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,34 @@
+vector<int> failTable(string s ) {
+    int i,j;
+    i = 0; 
+    j = 1; 
+    vector<int >v(sz(s) ) ;
+    for( ; j<sz(s) ;j++ ) {
+        if(s[i]==s[j] ) {
+            v[j] = i+1;
+            i++;  
+
+        }  else {
+            i = v[j-1] ;
+            while (1) {
+                if(i==0 ) {
+                    if(s[i]!=s[j] ) v[j] = 0 , i=0; 
+                    else v[j] = i+1, i++; 
+                    break ;  
+                } else {    
+                    if(s[i]==s[j] ) {
+                        v[j] =i+1 ; i++; 
+                        break; 
+                    }
+                    i = v[i-1] ;               
+
+                }
+            }
+        }
+    }
+
+
+    return v ; 
+}   
+// input abaab
+// output 00112
