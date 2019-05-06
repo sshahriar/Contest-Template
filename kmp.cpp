a2cdcace8b44b52375dec553e2c6f9d8bddbb709
shahriar <30356865+sshahriar@users.noreply.github.com>
2019-05-06 06:13:53 +0600

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,16 +1,15 @@
 // returns kmp fail table for given s
 // works for string vector
 
-vector<int> failTable(vector<int> s ) {
-    int i,j;
-    i = 0; 
-    j = 1; 
+// returns failtable for given string or vector
+// input abaab
+// output 00112
+vector<int> failTable(vector<int> s ) { // change vector to string for string matching 
+    int i,j; i = 0; j = 1; 
     vector<int >v(sz(s) ) ;
     for( ; j<sz(s) ;j++ ) {
         if(s[i]==s[j] ) {
-            v[j] = i+1;
-            i++;  
-
+            v[j] = i+1;  i++;  
         }  else {
             i = v[j-1] ;
             while (1) {
@@ -29,44 +28,28 @@ vector<int> failTable(vector<int> s ) {
             }
         }
     }
-
-
     return v ; 
 }   
-// input abaab
-// output 00112
 
-bool match(vector<int>v  ,vector<int> s , vector<int>  p  ) {
+// here s = actual string
+// and p =  pattern to be matched 
+bool match(vector<int>v  ,vector<int> s , vector<int>  p  ) { //change last two to string for str match
     int f= 0 , curr= 0 , len= sz(v) ;
-
-    // dbg(s, p) ;
-
     rep(i , sz(p  ) ) {
         if(i==0) continue; 
         if(p[i] == s[curr ]) { 
             curr++ ; 
             if(curr==len) return 1 ; 
-        
         } else{
-            //dbg("not matjc")  ;
             while(curr>0) {
-
                 curr = v[curr-1]  ;
                 if(p[i] == s[curr] ) {
                     curr++ ; 
                     break ; 
-
                 } else {
-
                 }   
-
             }
- 
         }
-
-       // dbg(i, curr );
-    }
-
     return 0 ; 
 }
 
