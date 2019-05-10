226066f5c3c9f61fb100e3bb4164b61a9508f535
shahriar <30356865+sshahriar@users.noreply.github.com>
2019-05-10 06:18:45 +0600

    Update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -54,3 +54,32 @@ bool match(vector<int>v  ,vector<int> s , vector<int>  p  ) {
         }
     return 0 ; 
 } 
+    
+    
+    
+///// another implementation 
+https://codeforces.com/blog/entry/66943 problem d  
+void init(char s[], int n, int kmp[], int nxt[][26])
+{
+    kmp[1] = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        int cur = kmp[i - 1];
+        while (cur > 0 && s[cur + 1] != s[i])
+            cur = kmp[cur];
+        if (s[cur + 1] == s[i])
+            ++cur;
+        kmp[i] = cur;
+    }
+
+    for (int i = 0; i <= n; i++)
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            int cur = i;
+            while (cur > 0 && s[cur + 1] != c)
+                cur = kmp[cur];
+            if (s[cur + 1] == c)
+                ++cur;
+            nxt[i][c - 'a'] = cur;
+        }
+}
