3daa66b9de3c3472612116eb4ca5072ef8d78cd4
shahriar <30356865+sshahriar@users.noreply.github.com>
2020-10-16 11:11:44 +0600

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -14,26 +14,15 @@ void init() {
 //itration 
 
 int   fact[ maxn ]  ,inv[ maxn] ;
+int n , m ;
 void  process() {    
     // mod = mod2; 
     fact[0]=1LL;
-    forn(i, 1 ,  maxn-1 ) fact[i] = mul(fact[i-1], i*1LL )  ;  
-    // rep(i, maxn ) inv[i ] =  invmod( fact[i] ,mod); 
+    forn(i, 1 ,  maxn-1 ) fact[i] = mul(fact[i-1],i) ;  
   
-   // dbg_a(fact ) ;
 }
- 
-int ncr (ll n ,ll r  ) {
- 
-    if( n-r < 0 ) return   0 ; 
-    ll nr =     invmod(fact[n-r] ,mod ) ;       //inv[n-r ] ; 
-    n = fact[n ] ; 
-    r =   invmod(fact[r] ,mod ) ;                //inv[r ] ;
- 
-    //dbg(n,r ,nr )    ;
- //   dbg(xy, t ,mx,my ) ; 
-    return mul(n, mul(r,nr) )   ; 
- 
+int ncr(int A,int B){
+  return fact[A]*bigmod(fact[A-B]*fact[B]%mod,mod-2)%mod;
 }
 /////////////////////
 int   fact[ maxn ]  ,inv[ maxn] ;
