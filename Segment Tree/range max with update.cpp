a01dc191e02959766c185bedafbd3660a7f93236
shahriar <30356865+sshahriar@users.noreply.github.com>
2020-10-27 19:53:41 +0600

    Update range max with update.cpp

--- Segment Tree/range max with update.cpp
+++ Segment Tree/range max with update.cpp
@@ -1,9 +1,9 @@
-int  tree[maxn*4]  , lazy[maxn*4 ]  ; 
+ int  tree[maxn*4]  , lazy[maxn*4 ]  ; 
 void push(int id,int l, int r ) {
     tree[id] = max( lazy[id] ,  tree[id]  ) ;
     if( l!=r) {
-        lazy[id*2] =  max(lazy[id] , lazy[id*2] ) ;
-        lazy[id*2+1 ] =  max(lazy[id] , lazy[id*2+1] ) ;
+        lazy[id<<1] =  max(lazy[id] , lazy[id<<1] ) ;
+        lazy[id<<1|1 ] =  max(lazy[id] , lazy[id<<1|1] ) ;
     }
 }
 void upd(int id,int l, int r , int s ,int e  , int val) {
@@ -15,21 +15,28 @@ void upd(int id,int l, int r , int s ,int e  , int val) {
         return ; 
     } 
     int mid = (l+r)/2  ; 
-    upd(id*2, l,mid , s,e , val  ); 
-    upd(id*2+1 , mid+1 ,r , s, e, val ) ;  
-    tree[id] = max(tree[id*2] , tree[id*2+1  ]  ) ; 
+    upd(id<<1, l,mid , s,e , val  ); 
+    upd(id<<1|1, mid+1 ,r , s, e, val ) ;  
+    tree[id] = max(tree[id<<1] , tree[id<<1|1]  ) ; 
 
 }
-int query(int id ,int l, int r , int s,int e ) {
+int query(int id ,int l, int r , int s,int e ) { 
     push(id, l,r) ; 
     if(l>e or r<s ) return  0 ;
-    if(l>=s and  r<=  e) {
+    if(l>=s and  r<=  e) { 
         return tree[id]; 
     } 
     int mid = (l+r)/2 , x,  y ; 
-   
-    x =  query(id*2, l,mid , s,e   ); 
-    y = query(id*2+1 , mid+1 ,r , s, e  ) ;  
+    x = query(id<<1, l,mid , s,e   ); 
+    y = query(id<<1|1 , mid+1 ,r , s, e  ) ;  
     return  max(x, y) ;  
+
+}
+ 
+void upd(int l  , int  val  ) {
+    upd(1,1,n,l,l , val )  ; 
+}
+int getmax( int l,int r )  {
+    return  query(1, 1, n, l,r )  ;  
 
 }
