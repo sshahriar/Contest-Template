266e7b5d7cb0d4a5480b02b1ed4bf242c28a6a4d
shahriar <30356865+sshahriar@users.noreply.github.com>
2020-11-09 01:02:22 +0600

    Update and rename bit_Seive.cpp to prime check.cpp

---
+++ prime check.cpp
@@ -0,0 +1,10 @@
+//   upto 3e8 works fine ...
+itset< maxn  >isPrime  ; 
+int cs = 0;
+void  seive(  )  {
+    for(int i=3 ;i*i < maxn ;i+=2)  if(!isPrime[i])  {
+        for(int j=i*i;j<maxn ; j+= (i<<1) ) 
+            isPrime[j] = 1 ; 
+    }   
+    isPrime[1] =isPrime[0] =  1 ;  
+} 
