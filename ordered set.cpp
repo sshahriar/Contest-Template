8e16b312373bb60cc3b3c791e83a6c095a316b06
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-01-31 15:36:05 +0600

    Create ordered set.cpp

---
+++ ordered set.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type , less<T> , rb_tree_tag , tree_order_statistics_node_update> ;
+ 
+	ordered_set<int>s ;
+
+int main()
+{
+ 
+	return 0 ;
+}		
