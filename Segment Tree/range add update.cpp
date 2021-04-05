1d7aec84f8196c32d97b1beaab9ddf98d2d07c74
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-04-06 01:18:12 +0600

    Update range add update.cpp

--- Segment Tree/range add update.cpp
+++ Segment Tree/range add update.cpp
@@ -1,69 +1,5 @@
- 
-#include <bits/stdc++.h> 
-using namespace std; 
-#define ff              first
-#define ss              second
-#define pb(x)           push_back(x)
-#define mp              make_pair
-#define sz(x)           (int)x.size()
-#define mem(x ,y)       memset(x , y , sizeof x )
-#define all(a )         a.begin() , a.end()  
-#define pii             pair<int,int >
-#define endl            "\n"  
-#define forn(i,x,y )    for(int i= x; i<=y ; i++ )  
-#define ford(i,x,y )    for(int i= x; i>=y ; i-- )  
-#define rep(i,n )       for(int i= 0; i<n ; i++ ) 
-#define repi(i,n )      for(int i= 1; i<=n ; i++ ) 
-#define repit(i, c)     for( __typeof((c).begin()) i = (c).begin(); i != (c).end();++i )
-#ifndef ONLINE_JUDGE           
-    #define dbg(args...)    do {   cout << #args << ' ' ;  print(args); } while(0); cerr<< endl ;
-#else 
-    #define dbg(args...)   ; 
-#endif 
-typedef long long ll  ;
-typedef unsigned long long ull  ;
-ll  mod =  1e9+7, mod2 =     998244353;
-int setbit(int n, int pos  ) { return n = n|(1LL<< pos) ; }
-int resetbit(int n,int pos ) { return n = n & ~(1LL<<pos ); }
-bool checkbit(int n,int pos) { return (bool ) (n&(1LL<<pos))  ; }
-template<typename T>T add(T x, T y, T mod = mod ){ x%= mod ;  y%= mod ;   return (x+y)%mod ;  }
-template<typename T>T sub(T x, T y, T mod = mod ){ return ((x-y)%mod+mod )%mod;  }
-template<typename T>T mul(T x, T y, T mod = mod ){ x %= mod ; y %= mod ; return (x*y)%mod  ;  }
-template<typename T> void print(const T& v) {    cout << v << ' ' ;}
-template<typename T1,typename... T2> void print( const T1& first, const T2&... rest ){ print(first); print(rest...) ;}
-template<typename T> void dbg_a(T a[] ,int n=10 ) {cerr << "[ "; for(int i=0;i <= n ; i++ )cerr<<a[i]<<' ' ; cerr<< " ]" <<  endl; } 
-template<typename F,typename S>ostream& operator<<( ostream& os, const pair< F, S > & p ){return os << "[ " << p.first << ", " << p.second << " ]";}
-template<typename T>ostream &operator<<(ostream & os, const vector< T > &v ){os << "[ "; for(int i=0; i<sz(v) ; i++ )  os << v[i] << ' ' ; return os << " ]\n" ; } 
-template<typename T>ostream &operator<<(ostream & os, const map< T ,T> &Map ){os << "[ "; repit(it , Map ) os << "[" <<(*it).ff << ' ' << (*it).ss << "] "    ; return os << "]\n" ; } 
-template<typename T>ostream &operator<<(ostream & os, const set< T > &Set  ){os << "[ "; repit(it , Set ) os <<  *it  << ' ' ; return os << " ]\n" ; }  
-template<typename T>ostream &operator<<(ostream & os, const multiset< T > &Set) {os << "[ "; repit(it , Set )os <<  *it  << ' ' ; return os << " ]\n" ; }  
-template < typename T > 
-T powmod(T x,T y  ,T mod = mod ) {
-    T r = 1LL;
-    while(y){
-        if(y&1)
-            r = (r * x) % mod;
-        y >>= 1;
-        x = (x * x) % mod;
-    }
-    return r;
-}
- 
-template < typename T > 
-T  invmod( T x , T m = mod) {  
-    return powmod(x,m-2 , m) ;   
-}
 
-
-#define  int  ll      
-const int maxn  = (int)  3e5+  123;
-const int inf  =  1e16; //0xc0  ;//0x3f ;    
-/////////////////////////////////////////////////
-  
-int n , m  ,  sum ;  
-int a[maxn  ] ; 
 int tree[maxn*4]  , lazy[maxn* 4]  ;
-
 void push(int id ,int l  , int r )  {
     if(lazy[id]< 0 ) {
         lazy[id] =  0 ;  
@@ -83,6 +19,7 @@ void push(int id ,int l  , int r )  {
 void  bld(int id,int l, int r )  {
     if( l>r) return  ; 
     if(l==r )  {
+        tree[id] = a[l] ; 
         return ;
     }
     int mid  = (l+r)/2  , lt=  id*2   ; 
@@ -124,49 +61,4 @@ void  add( int  l,int r ,int val )  {
 }
 int get( int  l,int r  )  {
     return query(1, 1,n ,l, r  ) ; 
-}
-
-void  solve() { 
-
-    int  x, y , w ,  z ,q  ,  tot = 0  , ans = 0 , mx =0 , mn     ;     
-    
-    cin >> n ;
-    rep( i , n ) cin  >> a[ i ] ;  
-
-    sort(all(a,  a+ n))  ;  b 
-
-    forn(i,l,r )cnt +=  a[i] ;     
-    rep(i,cnt) a[i+l]++
-    
-
-
-    cout << ans<< end  ; 
-
-
-
-
-
-
-}        
- //  before submit  -> //  check case 1 
-signed main() {     
-         freopen("in.txt" , "r" , stdin ) ;  
-    
-
-    ios :: sync_with_stdio(false);   cin.tie(0); cout.tie(0)  ;      
-    cout << fixed << setprecision( 12) ; 
-    int t = 1 , cs =0   ;     
-  
-    cin >> t ;  
-    while(t-- )   {   
-        // cout <<  "Case #"<<  ++cs << ": " ; 
-        solve()  ;   
-    }  
-    return 0 ;      
-}    
-
-  // segment 
-// kmp 
-// lca   
-// miller rabin  
-// 
+} 
